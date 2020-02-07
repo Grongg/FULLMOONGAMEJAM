b44398c075e29cdd37b3f740883e48c4aff4d15b
Arthur Lambert <39805498+ArthurLambert@users.noreply.github.com>
2020-02-07 20:01:58 +0100

    Player movement

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -5,15 +5,30 @@ using System;
 
 public class PlayerScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float speed = 6.0f;
+    public float jumpSpeed = 0.08f;
+    public float gravity = 10.0f;
+
+    private Vector2 moveDirection = new Vector2(0, 0);
+    private CharacterController characterController;
+
     void Start()
     {
-        
+        characterController = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        moveDirection.x = Input.GetAxis("Horizontal") * Time.deltaTime;
+        moveDirection.y += -gravity * Time.deltaTime;
+        if (characterController.isGrounded)
+        {
+            Debug.Log(characterController.isGrounded);
+            if (Input.GetButtonDown("Jump"))
+            {
+                moveDirection.y = jumpSpeed;
+            }
+        }
+        characterController.Move(moveDirection);
     }
 }
