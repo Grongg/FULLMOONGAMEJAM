9451bac87e674ed9e76beb89859e2120d778a8c7
Alexandru-Burla <37006672+Alexandru-Burla@users.noreply.github.com>
2020-02-08 08:29:35 +0100

    [ADD] Menu option with "escape"

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    public GameObject MyGame;
+    public GameObject Pause;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("escape"))
+        {
+            MyGame.SetActive(false);
+            Pause.SetActive(true);
+        }
+    }
+}
---
+++ Assets/Scripts/Pause.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void QuitGame() // Fonction that is attached to QuitButton to quit game
+    {
+        Debug.Log("QUIT"); // log "Quit" to see that it's work in the unity creator
+        Application.Quit(); // quit the game
+    }
+}
