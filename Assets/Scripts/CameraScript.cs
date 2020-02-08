620d15e0dc33c598460db8cd2e25c267877d4f76
Arthur Lambert <39805498+ArthurLambert@users.noreply.github.com>
2020-02-08 13:17:57 +0100

    Camera folowing the player ??

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    public Transform playerPosition;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.position = new Vector3(playerPosition.position.x, playerPosition.position.y, -10);
+    }
+}
