795ec820d83a441a58da84273c0d55a7ec2b3c41
grong <lionel.de-sousa@epitech.eu>
2020-02-08 18:05:21 +0100

    [ADD] Data collector with push and pull global value

---
+++ Assets/Scripts/DataCollector.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class DataCollector
+{
+    private static bool state;
+    public static bool push
+    {
+        get;
+        set;
+    }
+    public static bool pull
+    {
+        get;
+        set;
+    }
+}
