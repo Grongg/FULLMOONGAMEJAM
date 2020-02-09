5a32e80805aa09319e66ed32296b5cccd529d1c7
grong <lionel.de-sousa@epitech.eu>
2020-02-09 15:14:14 +0100

    [ADD] tilemap

---
+++ Assets/Material/Tilemap/AllTiles.tsx
@@ -0,0 +1,187 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.2" name="AllTiles" tilewidth="32" tileheight="32" tilecount="61" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="32" height="32" source="TilemapSources/Barriere.png"/>
+ </tile>
+ <tile id="1">
+  <image width="32" height="32" source="TilemapSources/Barriere2.png"/>
+ </tile>
+ <tile id="2">
+  <image width="32" height="32" source="TilemapSources/Barriere3.png"/>
+ </tile>
+ <tile id="3">
+  <image width="32" height="32" source="TilemapSources/Barriere4.png"/>
+ </tile>
+ <tile id="4">
+  <image width="32" height="32" source="TilemapSources/champignon_de_mes_couilles.png"/>
+ </tile>
+ <tile id="5">
+  <image width="32" height="32" source="TilemapSources/champignon_de_mes_couilles2.png"/>
+ </tile>
+ <tile id="6">
+  <image width="32" height="32" source="TilemapSources/champignon_de_mes_couilles3.png"/>
+ </tile>
+ <tile id="7">
+  <image width="32" height="32" source="TilemapSources/champignon_de_mes_couilles4.png"/>
+ </tile>
+ <tile id="8">
+  <image width="32" height="32" source="TilemapSources/flaque_d_eau_sa_mere.png"/>
+ </tile>
+ <tile id="9">
+  <image width="32" height="32" source="TilemapSources/fond_noir.png"/>
+ </tile>
+ <tile id="10">
+  <image width="32" height="32" source="TilemapSources/Herbe.png"/>
+ </tile>
+ <tile id="11">
+  <image width="32" height="32" source="TilemapSources/Herbe2.png"/>
+ </tile>
+ <tile id="12">
+  <image width="32" height="32" source="TilemapSources/Herbe3.png"/>
+ </tile>
+ <tile id="13">
+  <image width="32" height="32" source="TilemapSources/Herbe4.png"/>
+ </tile>
+ <tile id="14">
+  <image width="32" height="32" source="TilemapSources/i_dont_know.png"/>
+ </tile>
+ <tile id="15">
+  <image width="32" height="32" source="TilemapSources/i_dont_know2.png"/>
+ </tile>
+ <tile id="16">
+  <image width="32" height="32" source="TilemapSources/i_dont_know3.png"/>
+ </tile>
+ <tile id="17">
+  <image width="32" height="32" source="TilemapSources/i_dont_know4.png"/>
+ </tile>
+ <tile id="18">
+  <image width="32" height="32" source="TilemapSources/Lierre.png"/>
+ </tile>
+ <tile id="19">
+  <image width="32" height="32" source="TilemapSources/Lierre2.png"/>
+ </tile>
+ <tile id="20">
+  <image width="32" height="32" source="TilemapSources/Lierre3.png"/>
+ </tile>
+ <tile id="21">
+  <image width="32" height="32" source="TilemapSources/Lierre4.png"/>
+ </tile>
+ <tile id="22">
+  <image width="32" height="32" source="TilemapSources/Pierre.png"/>
+ </tile>
+ <tile id="23">
+  <image width="32" height="32" source="TilemapSources/Pierre2.png"/>
+ </tile>
+ <tile id="24">
+  <image width="32" height="32" source="TilemapSources/Pierre3.png"/>
+ </tile>
+ <tile id="25">
+  <image width="32" height="32" source="TilemapSources/Pierre4.png"/>
+ </tile>
+ <tile id="26">
+  <image width="32" height="32" source="TilemapSources/plaque_dareation.png"/>
+ </tile>
+ <tile id="27">
+  <image width="32" height="32" source="TilemapSources/plaque_dareation2.png"/>
+ </tile>
+ <tile id="28">
+  <image width="32" height="32" source="TilemapSources/Putain_daimant.png"/>
+ </tile>
+ <tile id="29">
+  <image width="32" height="32" source="TilemapSources/putain_daimant_2.png"/>
+ </tile>
+ <tile id="30">
+  <image width="32" height="32" source="TilemapSources/putain_daimant_2_bleu.png"/>
+ </tile>
+ <tile id="31">
+  <image width="32" height="32" source="TilemapSources/putain_daimant_3.png"/>
+ </tile>
+ <tile id="32">
+  <image width="32" height="32" source="TilemapSources/putain_daimant_3_bleu.png"/>
+ </tile>
+ <tile id="33">
+  <image width="32" height="32" source="TilemapSources/Putain_daimant_4.png"/>
+ </tile>
+ <tile id="34">
+  <image width="32" height="32" source="TilemapSources/Putain_daimant_4_bleu.png"/>
+ </tile>
+ <tile id="35">
+  <image width="32" height="32" source="TilemapSources/Putain_daimant_bleu.png"/>
+ </tile>
+ <tile id="36">
+  <image width="32" height="32" source="TilemapSources/sol.png"/>
+ </tile>
+ <tile id="37">
+  <image width="32" height="32" source="TilemapSources/sol__changement_de_temps_deux_cotes_bas_droite.png"/>
+ </tile>
+ <tile id="38">
+  <image width="32" height="32" source="TilemapSources/sol__changement_de_temps_deux_cotes_bas_gauche.png"/>
+ </tile>
+ <tile id="39">
+  <image width="32" height="32" source="TilemapSources/sol__changement_de_temps_deux_cotes_gauche.png"/>
+ </tile>
+ <tile id="40">
+  <image width="32" height="32" source="TilemapSources/sol_changement_de_temps.png"/>
+ </tile>
+ <tile id="41">
+  <image width="32" height="32" source="TilemapSources/sol_changement_de_tempsl_deux_cotes.png"/>
+ </tile>
+ <tile id="42">
+  <image width="32" height="32" source="TilemapSources/sol_changement_de_tempsl_trois_cotes.png"/>
+ </tile>
+ <tile id="43">
+  <image width="32" height="32" source="TilemapSources/sol_changement_de_tempsl_trois_cotes_bas.png"/>
+ </tile>
+ <tile id="44">
+  <image width="32" height="32" source="TilemapSources/sol_changement_de_tempsl_trois_cotes_droite.png"/>
+ </tile>
+ <tile id="45">
+  <image width="32" height="32" source="TilemapSources/sol_changement_de_tempsl_trois_cotes_gauche.png"/>
+ </tile>
+ <tile id="46">
+  <image width="32" height="32" source="TilemapSources/sol_temps_normal.png"/>
+ </tile>
+ <tile id="47">
+  <image width="32" height="32" source="TilemapSources/sol_temps_normal_deux_cotes.png"/>
+ </tile>
+ <tile id="48">
+  <image width="32" height="32" source="TilemapSources/sol_temps_normal_deux_cotes_bas_droite.png"/>
+ </tile>
+ <tile id="49">
+  <image width="32" height="32" source="TilemapSources/sol_temps_normal_deux_cotes_bas_gauche.png"/>
+ </tile>
+ <tile id="50">
+  <image width="32" height="32" source="TilemapSources/sol_temps_normal_deux_cotes_gauche.png"/>
+ </tile>
+ <tile id="51">
+  <image width="32" height="32" source="TilemapSources/sol_temps_normal_trois_cotes.png"/>
+ </tile>
+ <tile id="52">
+  <image width="32" height="32" source="TilemapSources/sol_temps_normal_trois_cotes_bas.png"/>
+ </tile>
+ <tile id="53">
+  <image width="32" height="32" source="TilemapSources/sol_temps_normal_trois_cotes_droite.png"/>
+ </tile>
+ <tile id="54">
+  <image width="32" height="32" source="TilemapSources/sol_temps_normal_trois_cotes_gauche.png"/>
+ </tile>
+ <tile id="55">
+  <image width="32" height="32" source="TilemapSources/sol_terre.png"/>
+ </tile>
+ <tile id="56">
+  <image width="32" height="32" source="TilemapSources/sol_terre2.png"/>
+ </tile>
+ <tile id="57">
+  <image width="32" height="32" source="TilemapSources/sol_terre3.png"/>
+ </tile>
+ <tile id="58">
+  <image width="32" height="32" source="TilemapSources/sol_terre4.png"/>
+ </tile>
+ <tile id="59">
+  <image width="32" height="32" source="TilemapSources/un_arbre_random.png"/>
+ </tile>
+ <tile id="60">
+  <image width="32" height="32" source="TilemapSources/vitre.png"/>
+ </tile>
+</tileset>
